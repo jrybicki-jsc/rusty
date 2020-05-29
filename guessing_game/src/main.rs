35e8a6303a64ad842f1a121281396f7e2ada9c04
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-05-29 09:31:28 +0200

    workign exxample

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,35 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
+   
+    loop { 
     println!("Please input your guess.");
-
     let mut guess = String::new(); //by default variables are immutable
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("Your guess was: {}", guess);
+    let guess: u32 = match guess.trim().parse() {
+           Ok(num) => num,
+           Err(_) => {
+              println!("Please enter a number");
+              continue;
+           },
+    };
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too little..."),
+        Ordering::Greater => println!("Too much!"),
+        Ordering::Equal => {
+               println!("You won!");
+               break;
+        },
+    }
+    }
 }
