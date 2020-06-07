9feef7ea499ee8e32b272ae94c27c928902299e2
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-06-07 11:33:35 +0200

    libs

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,12 +1,43 @@
 mod front_of_house {
-   mod hosting {
-      fn add_to_whitelist() {}
-      fn seat_at_table() {}
-   }
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
 
-   mod serving {
-      fn take_order() { }
-      fn serve_order() {}
-      fn take_payement() {}
-   }
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payement() {}
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = front_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    //meal.seasonal_fruit = String::from("Blueberries");
+    println!("I would like {} toast please", meal.toast);
+
+    let order1 = front_of_house::Appetizer::Soup;
 }
