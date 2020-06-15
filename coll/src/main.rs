94488310e7c7db9d8c44cabf7157905dbea33d91
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-06-15 11:30:47 +0200

    colls

--- coll/src/main.rs
+++ coll/src/main.rs
@@ -1,6 +1,49 @@
+use std::collections::HashMap;
+
+
+fn mean(v: &Vec<i32>) -> f64{
+
+   //let v = vec![1, 2, 3, 4, 5];
+   let mut sum:i32 = 0;
+   let mut count:i32 = 0;
+   for i in v {
+       sum= sum + i;
+       count = count + 1;
+   }
+
+   (sum as f64)/(count as f64)
+}
+
+fn mode(v: &Vec<i32>) -> i32 {
+   let mut scores= HashMap::new();
+
+   for i in v {
+      let count = scores.entry(i).or_insert(0);
+      *count+=1;
+   }
+
+   let mut occurs =0;
+
+   for (key, value) in &scores {
+       if *value > occurs {
+          occurs = **key;
+       }
+    }
+
+   occurs
+
+} 
 
 
 fn main() {
+    let myvec = vec![1, 2, 3, 4, 5, 6, 2, 3, 3];
+    let m = mean(&myvec);
+    println!("Mean={}", m);
+
+    let mp = mode(&myvec);
+    println!("{:?}", myvec);
+    println!("Mode: {}", mp);
+
     let mut v = Vec::new();
     v.push(5);
     v.push(6);
--- strs/src/main.rs
+++ strs/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let data = "initial contents";
     let s = data.to_string();
@@ -21,4 +23,35 @@ fn main() {
     for b in ss.bytes() {
         println!("{}", b);
     } 
+
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 30);
+
+    let team_name = String::from("Black");
+    scores.insert(team_name, 20);
+    //println!("Invalid team name: {}", team_name);
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    match score {
+         Some(x) => println!("{} score is: {}", team_name, x),
+         None => println!("There is no {} team", team_name),
+    }
+
+    for (key, value) in &scores {
+         println!("{}: {}", key, value);
+    }
+
+    scores.insert(String::from("Blue"), 20);
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Orange")).or_insert(59);
+    scores.entry(String::from("Blue")).or_insert(21);
+    println!("{:?}", scores);
+
+    let count = scores.entry(String::from("Blue")).or_insert(0);
+    *count+=21;
+    println!("{:?}", scores);
+    
 }
