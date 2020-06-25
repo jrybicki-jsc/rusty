d314c03242451d1b34c875af37fe1c9b4fe88b3e
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-06-25 09:21:05 +0200

    iters

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -51,12 +51,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+ 
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get filename"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITVE").is_err();
 
 
