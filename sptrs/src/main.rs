499fac8e4543b78eecd0cace4c1c428d5ab9284d
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-06-29 20:32:25 +0200

    deref

--- sptrs/src/main.rs
+++ sptrs/src/main.rs
@@ -11,4 +11,38 @@ fn main() {
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    //dref
+    let x = 5;
+    let y = &x;
+    
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    //assert_eq!(5, y);
+
+    let y = Box::new(x);
+    assert_eq!(5, *y);  //equals despite being a reference to a box
+
+    let y = MyBox::new(x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+   fn new(x: T) -> MyBox<T> {
+      MyBox(x)
+   }
+
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+
 }
