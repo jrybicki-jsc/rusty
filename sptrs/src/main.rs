a71db6b161067a9ed04a5374c8f41d13352da32e
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-06-30 19:43:36 +0200

    fmts

--- sptrs/src/main.rs
+++ sptrs/src/main.rs
@@ -1,10 +1,9 @@
 use crate::List::{Cons, Nil};
 
 enum List {
-   Cons(i32, Box<List>),
-   Nil,
+    Cons(i32, Box<List>),
+    Nil,
 }
-
 
 fn main() {
     let b = Box::new(5); // store i32 on the heap
@@ -15,13 +14,13 @@ fn main() {
     //dref
     let x = 5;
     let y = &x;
-    
+
     assert_eq!(5, x);
     assert_eq!(5, *y);
     //assert_eq!(5, y);
 
     let y = Box::new(x);
-    assert_eq!(5, *y);  //equals despite being a reference to a box
+    assert_eq!(5, *y); //equals despite being a reference to a box
 
     let y = MyBox::new(x);
     assert_eq!(5, *y);
@@ -29,7 +28,7 @@ fn main() {
     let me = MyBox::new(String::from("Rust"));
     hello(&me);
 
-    let c = CustomSmartPointer{
+    let c = CustomSmartPointer {
         data: String::from("my stuff"),
     };
 
@@ -37,33 +36,32 @@ fn main() {
 }
 
 fn hello(name: &str) {
-   println!("Hello, {}!", name);
+    println!("Hello, {}!", name);
 }
 
 struct CustomSmartPointer {
-   data: String,
+    data: String,
 }
 
 impl Drop for CustomSmartPointer {
-   fn drop(&mut self) {
-      println!("Dropping custom sm. pointer with data {}!", self.data);
-   }
+    fn drop(&mut self) {
+        println!("Dropping custom sm. pointer with data {}!", self.data);
+    }
 }
 
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
-   fn new(x: T) -> MyBox<T> {
-      MyBox(x)
-   }
-
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
 impl<T> Drop for MyBox<T> {
-    fn drop(&mut self) { 
+    fn drop(&mut self) {
         println!("Dropping mybox...");
     }
-} 
+}
 
 use std::ops::Deref;
 
@@ -73,5 +71,4 @@ impl<T> Deref for MyBox<T> {
     fn deref(&self) -> &T {
         &self.0
     }
-
 }
