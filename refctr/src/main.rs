f410b83e9dbacbb0da57b5842874267312f7b498
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-03 20:37:49 +0200

    refco

--- refctr/src/main.rs
+++ refctr/src/main.rs
@@ -63,3 +63,37 @@ where
  
    }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+       sent_messages: RefCell<Vec<String>>,
+    }
+
+   impl MockMessenger {
+       fn new() -> MockMessenger {
+             MockMessenger {
+                  sent_messages: RefCell::new(vec![]),
+              }
+       }
+   }
+
+   impl Messenger for MockMessenger {
+       fn send(&self, message: &str) {
+          self.sent_messages.borrow_mut().push(String::from(message));
+       }
+   }
+
+   #[test]
+   fn it_send_an_over_75() {
+      let mock_messenger = MockMessenger::new();
+      let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+      limit_tracker.set_value(80);
+      assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+   }
+}
