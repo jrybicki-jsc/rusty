1647575efc324ef405f49eddb69aa6cfa8be3896
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-05 20:55:41 +0200

    uupp

--- memle/src/main.rs
+++ memle/src/main.rs
@@ -4,26 +4,24 @@ use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 enum List {
-   Cons(i32, RefCell<Rc<List>>),
-   Nil,
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
 }
 
-
 impl List {
-   fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-      match self {
-         Cons(_, item) => Some(item),
-         Nil => None,
-      }
-   }
-  
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Node {
-   value: i32,
-   children: RefCell<Vec<Rc<Node>>>,
-   parent: RefCell<Weak<Node>>,
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>,
 }
 
 fn main() {
@@ -39,30 +37,42 @@ fn main() {
     println!("A next item is {:?}", b.tail());
 
     if let Some(link) = a.tail() {
-       *link.borrow_mut() = Rc::clone(&b);
+        *link.borrow_mut() = Rc::clone(&b);
     }
 
     println!("Rc count  {}", Rc::strong_count(&a));
     println!("Rc countb {}", Rc::strong_count(&b));
 
-    let leaf = Rc::new(Node{
-          value: 3,
-          children: RefCell::new(vec![]),
-          parent: RefCell::new(Weak::new()),
-     });
-    println!("\nStrong: {}\nWeak: {}\n", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new()),
+    });
+    println!(
+        "\nStrong: {}\nWeak: {}\n",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 
     println!("Leaf parent: {:?}", leaf.parent.borrow().upgrade());
 
     let branch = Rc::new(Node {
-          value: 5, 
-          children: RefCell::new(vec![Rc::clone(&leaf)]),
-          parent: RefCell::new(Weak::new()),
-     });
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+        parent: RefCell::new(Weak::new()),
+    });
 
-     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-     println!("Leaf parent: {:?}", leaf.parent.borrow().upgrade());
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("Leaf parent: {:?}", leaf.parent.borrow().upgrade());
 
-     println!("\nStrong: {}\nWeak: {}\n", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
-      println!("Branch\nStrong: {}\nWeak: {}\n", Rc::strong_count(&branch), Rc::weak_count(&branch));
+    println!(
+        "\nStrong: {}\nWeak: {}\n",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+    println!(
+        "Branch\nStrong: {}\nWeak: {}\n",
+        Rc::strong_count(&branch),
+        Rc::weak_count(&branch)
+    );
 }
