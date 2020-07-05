6a04ec753d89c83abf598b6130e993b51669c556
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-05 20:55:06 +0200

    weaks

--- memle/src/main.rs
+++ memle/src/main.rs
@@ -1,6 +1,6 @@
 use crate::List::{Cons, Nil};
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 enum List {
@@ -23,6 +23,7 @@ impl List {
 struct Node {
    value: i32,
    children: RefCell<Vec<Rc<Node>>>,
+   parent: RefCell<Weak<Node>>,
 }
 
 fn main() {
@@ -47,12 +48,21 @@ fn main() {
     let leaf = Rc::new(Node{
           value: 3,
           children: RefCell::new(vec![]),
+          parent: RefCell::new(Weak::new()),
      });
+    println!("\nStrong: {}\nWeak: {}\n", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+
+    println!("Leaf parent: {:?}", leaf.parent.borrow().upgrade());
 
     let branch = Rc::new(Node {
           value: 5, 
           children: RefCell::new(vec![Rc::clone(&leaf)]),
+          parent: RefCell::new(Weak::new()),
      });
 
+     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+     println!("Leaf parent: {:?}", leaf.parent.borrow().upgrade());
 
+     println!("\nStrong: {}\nWeak: {}\n", Rc::strong_count(&leaf), Rc::weak_count(&leaf));
+      println!("Branch\nStrong: {}\nWeak: {}\n", Rc::strong_count(&branch), Rc::weak_count(&branch));
 }
