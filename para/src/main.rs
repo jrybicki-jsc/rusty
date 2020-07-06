7177913b12908fc9ce9fa72d1472d9df80c46840
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-06 20:19:04 +0200

    movs

--- para/src/main.rs
+++ para/src/main.rs
@@ -1,3 +1,22 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+   let v = vec![1, 2, 3];
+
+   let handle = thread::spawn(move || {
+       for i in 1..3 {
+           println!("Hi number {} from the thread", i);
+           println!("{:?}", v);
+       }
+   });
+    //move protects you from doing that:
+   //drop(v);
+
+   for i in 1..5 {
+       println!("Hi number {} from main", i);
+       thread::sleep(Duration::from_millis(1));
+   }
+
+   handle.join().unwrap();
 }
