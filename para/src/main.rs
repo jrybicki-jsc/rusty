0e35e3306260278314c9b0b8de0e404a83b80d58
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-09 21:36:52 +0200

    forgotten; ;

--- para/src/main.rs
+++ para/src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 
 
 fn main() {
@@ -60,4 +61,30 @@ fn main() {
    for recieved in rx {
           println!("Got {}!", recieved); 
    }
+
+
+  let m = Mutex::new(5);
+  {
+     let mut num = m.lock().unwrap();
+     *num = 6;
+  }
+
+  println!("m = {:?}", m); 
+  let counter = Arc::new(Mutex::new(0));
+  let mut handles = vec![];
+  for _ in 0..10 {
+      let counter = Arc::clone(&counter);
+      let handle = thread::spawn(move || {
+          let mut num = counter.lock().unwrap();
+          *num +=1;
+      });
+      handles.push(handle);
+  }
+
+  for handle in handles {
+      handle.join().unwrap();
+  }
+
+  println!("Result {}", *counter.lock().unwrap());
+
 }
