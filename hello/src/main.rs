21f687db0ec5fc465da85a4a01b44c727db5899d
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-19 09:22:43 +0200

    threads

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,14 +2,20 @@ use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(5);
 
     for stream in listener.incoming() {
        let stream = stream.unwrap();
-       handle_connection(stream);
+       pool.execute(|| {
+             handle_connection(stream);
+       });
    }
   
 }
@@ -21,11 +27,17 @@ fn handle_connection(mut stream: TcpStream) {
    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
    let get = b"GET / HTTP/1.1\r\n";
+   let sleep = b"GET /sleep HTTP/1.1\r\n";
+
    let response =  if buffer.starts_with(get) {
       let content = fs::read_to_string("hello.html").unwrap();
 
       format!("HTTP/1.1 200 OK\r\n\r\n{}", content)
-  } else {
+  } else if buffer.starts_with(sleep) {
+       thread::sleep(Duration::from_secs(5));
+       format!("HTTP/1.1 200 OK\r\n\r\n")
+   }
+   else {
       format!("HTTP/1.1 404 NOT FOUND\r\n\r\n")
    };
 
