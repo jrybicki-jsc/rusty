857bacb9e2ff11eab38787908b37287807fc6061
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-20 08:39:57 +0200

    multi

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
-use std::io::prelude::*;
-use std::net::TcpStream;
 use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
@@ -12,36 +12,33 @@ fn main() {
     let pool = ThreadPool::new(5);
 
     for stream in listener.incoming() {
-       let stream = stream.unwrap();
-       pool.execute(|| {
-             handle_connection(stream);
-       });
-   }
-  
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-   let mut buffer = [0; 1024];
-   stream.read(&mut buffer).unwrap();
-   
-   println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-   let get = b"GET / HTTP/1.1\r\n";
-   let sleep = b"GET /sleep HTTP/1.1\r\n";
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-   let response =  if buffer.starts_with(get) {
-      let content = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-      format!("HTTP/1.1 200 OK\r\n\r\n{}", content)
-  } else if buffer.starts_with(sleep) {
-       thread::sleep(Duration::from_secs(5));
-       format!("HTTP/1.1 200 OK\r\n\r\n")
-   }
-   else {
-      format!("HTTP/1.1 404 NOT FOUND\r\n\r\n")
-   };
+    let response = if buffer.starts_with(get) {
+        let content = fs::read_to_string("hello.html").unwrap();
 
-   stream.write(response.as_bytes()).unwrap();
-   stream.flush().unwrap();
-   
+        format!("HTTP/1.1 200 OK\r\n\r\n{}", content)
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        format!("HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+        format!("HTTP/1.1 404 NOT FOUND\r\n\r\n")
+    };
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
