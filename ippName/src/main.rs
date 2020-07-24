db51bb866578136cd47453bb71a78a35fbd409af
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-24 08:46:10 +0200

    bars

--- ippName/src/main.rs
+++ ippName/src/main.rs
@@ -1,7 +1,40 @@
 use rand::Rng;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
 use std::io;
 
+fn pmbc() -> Result<(), Box<dyn Error>> {
+    let filename = String::from("./f.txt");
+    let contents = fs::read_to_string(filename)?;
+
+    let mut letters = HashMap::new();
+
+    for c in contents.chars() {
+        if c.is_ascii_alphabetic() {
+            let count = letters.entry(c).or_insert(0);
+            *count += 1;
+        }
+    }
+
+    println!("{:?}", letters);
+
+    for (c, &number) in letters.iter() {
+        print!("{}:", c);
+        for _ in 0..number {
+            print!("{}", c);
+        }
+        print!("\n");
+    }
+
+    Ok(())
+}
+
 fn main() {
+    pmbc();
+
+    return;
+
     let first = vec!["Ann", "Bell", "John", "Worn", "Bill"];
     let last = vec!["Crab", "Milk", "Onerg", "Zonk", "Smack"];
 
