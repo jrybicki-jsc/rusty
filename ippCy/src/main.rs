82b88b07da566ef7ec8af0e41825c31100962eee
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-07-31 06:59:16 +0200

    raile

--- ippCy/src/main.rs
+++ ippCy/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn route_ci() {
     let ciphertext = "16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19";
     let cols = 4;
     let rows = 5;
@@ -30,6 +30,33 @@ fn main() {
             print!("{} ", get_ij(a, b, &matrix));
         }
     }
+}
+
+fn main() {
+    //railfence:
+    let plaintext = String::from("Let us cross over the river and rest under the shade of the trees");
+    //capitalize and remove spac
+    let plaintext:String = plaintext.to_uppercase().split_whitespace().collect();
+
+    let mut upper: Vec<char> = Vec::new();
+    let mut lower: Vec<char> = Vec::new();
+
+    for (p, o)  in plaintext.chars().enumerate() {
+        if p % 2 == 0 {
+           upper.push(o);
+        } else {
+           lower.push(o);
+        }
+        if (p> 0) && (p % 8 == 0) {
+           upper.push(' ');
+           lower.push(' ');
+        }
+    }
+
+    let mut upper =  upper.iter().collect::<String>();
+    let mut lower = lower.iter().collect::<String>();
+  
+    print!("{}\n{}", upper, lower); 
 }
 
 fn get_ij(i: usize, j: usize, matrix: &Vec<u8>) -> u8 {
