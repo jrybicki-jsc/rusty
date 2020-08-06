7b5a456afb8264a926928a002f3d37c970f60b0c
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-06 08:15:42 +0200

    fooling around

--- ippRat/src/main.rs
+++ ippRat/src/main.rs
@@ -1,3 +1,35 @@
+use std::fmt;
+
+enum Sex {
+    Male,
+    Female,
+}
+
+pub struct Rat {
+    weight: u16,
+    sex: Sex,
+}
+
+impl fmt::Display for Rat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.weight, self.sex)
+    }
+}
+
+impl fmt::Display for Sex {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Male => write!(f, "Male"),
+            _ => write!(f, "Female"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let r = Rat {
+        weight: 19,
+        sex: Sex::Male,
+    };
+
+    println!("Got rat {}", r);
 }
