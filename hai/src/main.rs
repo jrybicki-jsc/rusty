7dc80f97345405787b3144c616d3aa3631b67ce3
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-15 10:22:46 +0200

    haikudi

--- hai/src/main.rs
+++ hai/src/main.rs
@@ -13,11 +13,33 @@ fn main() {
     for word in contents.lines() {
         let split = word.split(" ").collect::<Vec<&str>>();
         if re.is_match(&split[0]) {
-            println!("Variant detected {}... skipping!", split[0]);
+//            println!("Variant detected {}... skipping!", split[0]);
             continue;
         }
         mydict.insert(split[0], split.len() - 1);
     }
 
     println!("Dict created {}", mydict.len());
+
+    let filename = String::from("./data/train.txt");
+    let contents = fs::read_to_string(filename).expect("Failed to open haiku");
+    println!("Got coropra: {}", contents.len());
+
+    let myword = String::from("someword,");
+    let w = myword.replace(|c: char| !c.is_alphabetic(), "");
+    let mut haikudict = Vec::new();
+    
+    for word in contents.split(" ") {
+        let mut w =  word.replace(|c: char| !c.is_alphabetic(), "");
+        w.make_ascii_lowercase();
+        if w.is_empty() {
+             continue;
+         }
+        if !mydict.contains_key(&w.as_str()) {
+           //println!("This word is missing in the dictionary {}", w);
+           haikudict.push(w);
+        }
+    }
+
+    println!("Haiku dict: {}", haikudict.len());
 }
