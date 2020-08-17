d4720abf1c2df2a60bea14e2819022b8a8d27956
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-17 07:38:26 +0200

    with loop

--- hai/src/main.rs
+++ hai/src/main.rs
@@ -1,16 +1,16 @@
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
+use std::io;
 
-fn count_syllables(sentence: &String, dictionary: &HashMap<&str, usize>) -> usize {
+fn count_syllables(sentence: &String, dictionary: &HashMap<String, usize>) -> usize {
     let mut ret: usize = 0;
-    let w = String::from("bla");
 
     for word in sentence.split(" ") {
         let mut w = word.replace(|c: char| !c.is_alphabetic(), "");
         w.make_ascii_lowercase();
 
-        match dictionary.get(&w.as_str()) {
+        match dictionary.get(&w) {
             Some(&syl) => {
                 ret += syl;
             }
@@ -23,7 +23,7 @@ fn count_syllables(sentence: &String, dictionary: &HashMap<&str, usize>) -> usiz
     ret
 }
 
-fn main() {
+fn read_cmu() -> HashMap<String, usize> {
     let filename = String::from("./data/cmudict.dict");
     let contents = fs::read_to_string(filename).expect("Failed to open dictionary");
     println!("Got dict: {}", contents.len());
@@ -37,15 +37,14 @@ fn main() {
             //            println!("Variant detected {}... skipping!", split[0]);
             continue;
         }
-        mydict.insert(split[0], split.len() - 1);
+        mydict.insert(split[0].to_string(), split.len() - 1);
     }
 
     println!("Dict created {}", mydict.len());
+    mydict
+}
 
-    let sen = String::from("Some sentence is empty");
-    let s = count_syllables(&sen, &mydict);
-    println!("Syllables count: {}", s);
-
+fn check_missing(mydict: HashMap<String, usize>) {
     let filename = String::from("./data/train.txt");
     let contents = fs::read_to_string(filename).expect("Failed to open haiku");
     println!("Got coropra: {}", contents.len());
@@ -58,11 +57,28 @@ fn main() {
         if w.is_empty() {
             continue;
         }
-        if !mydict.contains_key(&w.as_str()) {
+        if !mydict.contains_key(&w) {
             //println!("This word is missing in the dictionary {}", w);
             haikudict.push(w);
         }
     }
 
     println!("Haiku dict: {}", haikudict.len());
+}
+
+
+fn main() {
+    let mydict = read_cmu();
+
+    loop {
+        let mut sen = String::new();
+        io::stdin()
+            .read_line(&mut sen)
+            .expect("Failed to read line");
+
+        
+
+        let s = count_syllables(&sen, &mydict);
+        println!(">syllables {}", s);
+   }
 }
