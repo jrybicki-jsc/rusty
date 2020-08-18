45a8504d23a2c941127839a19725a53a4226f967
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-18 07:47:56 +0200

    relo

---
+++ hai/src/lib.rs
@@ -0,0 +1,46 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
+
+
+pub fn count_syllables(sentence: &String, dictionary: &HashMap<String, usize>) -> usize {
+    let mut ret: usize = 0;
+
+    for word in sentence.split(" ") {
+        let mut w = word.replace(|c: char| !c.is_alphabetic(), "");
+        w.make_ascii_lowercase();
+
+        match dictionary.get(&w) {
+            Some(&syl) => {
+                ret += syl;
+            }
+            _ => {
+                println!("Could not find {} in the dict", word);
+            }
+        };
+    }
+
+    ret
+}
+
+pub fn read_cmu(filename: String) -> HashMap<String, usize> {
+    //let filename = String::from("./data/cmudict.dict");
+    let contents = fs::read_to_string(filename).expect("Failed to open dictionary");
+    println!("Got dict: {}", contents.len());
+    let re = Regex::new(r"\w\(\d\)").unwrap();
+
+    let mut mydict = HashMap::new();
+
+    for word in contents.lines() {
+        let split = word.split(" ").collect::<Vec<&str>>();
+        if re.is_match(&split[0]) {
+            //            println!("Variant detected {}... skipping!", split[0]);
+            continue;
+        }
+        mydict.insert(split[0].to_string(), split.len() - 1);
+    }
+
+    println!("Dict created {}", mydict.len());
+    mydict
+}
+
--- hai/src/main.rs
+++ hai/src/main.rs
@@ -1,48 +1,8 @@
-use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 use std::io;
 
-fn count_syllables(sentence: &String, dictionary: &HashMap<String, usize>) -> usize {
-    let mut ret: usize = 0;
-
-    for word in sentence.split(" ") {
-        let mut w = word.replace(|c: char| !c.is_alphabetic(), "");
-        w.make_ascii_lowercase();
-
-        match dictionary.get(&w) {
-            Some(&syl) => {
-                ret += syl;
-            }
-            _ => {
-                println!("Could not find {} in the dict", word);
-            }
-        };
-    }
-
-    ret
-}
-
-fn read_cmu() -> HashMap<String, usize> {
-    let filename = String::from("./data/cmudict.dict");
-    let contents = fs::read_to_string(filename).expect("Failed to open dictionary");
-    println!("Got dict: {}", contents.len());
-    let re = Regex::new(r"\w\(\d\)").unwrap();
-
-    let mut mydict = HashMap::new();
-
-    for word in contents.lines() {
-        let split = word.split(" ").collect::<Vec<&str>>();
-        if re.is_match(&split[0]) {
-            //            println!("Variant detected {}... skipping!", split[0]);
-            continue;
-        }
-        mydict.insert(split[0].to_string(), split.len() - 1);
-    }
-
-    println!("Dict created {}", mydict.len());
-    mydict
-}
+use hai::{read_cmu, count_syllables};
 
 fn check_missing(mydict: HashMap<String, usize>) {
     let filename = String::from("./data/train.txt");
@@ -67,7 +27,7 @@ fn check_missing(mydict: HashMap<String, usize>) {
 }
 
 fn main() {
-    let mydict = read_cmu();
+    let mydict = read_cmu(String::from("./data/cmudict.dict"));
 
     loop {
         let mut sen = String::new();
