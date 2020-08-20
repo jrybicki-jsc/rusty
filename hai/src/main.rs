ce278c5a5caf88aa4501c19ea0f13487ad6c3ba6
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-20 09:29:13 +0200

    mmg

--- hai/src/main.rs
+++ hai/src/main.rs
@@ -1,17 +1,19 @@
 use std::collections::HashMap;
 use std::fs;
 use std::io;
+use rand::Rng;
+use rand::seq::SliceRandom;
 
 use hai::{count_syllables, read_cmu};
 
 fn clean_word(word: &str) -> String {
-    let mut w = word.replace(|c: char| !c.is_alphabetic(), "");
+    let mut w = word.replace(|c: char| !c.is_alphabetic() & !c.is_whitespace(), "");
     w.make_ascii_lowercase();
 
     w
 }
 
-fn alt_markov() {
+fn alt_markov() -> HashMap<(String, String), Vec<String>>{
     let filename = String::from("./data/train.txt");
     let contents = fs::read_to_string(filename).expect("Failed to open haiku");
     println!("Got coropra: {}", contents.len());
@@ -28,25 +30,15 @@ fn alt_markov() {
     let mut di: HashMap<(String, String), Vec<String>> = HashMap::new();
 
     for e in m {
-         let mytu = ((e.1).0.to_string(), (e.1).1.to_string());
+         let mytu = (clean_word(&(e.1).0.to_string()), clean_word(&(e.1).1.to_string()));
 
          let v = di.entry(mytu).or_insert(Vec::<String>::new());
-         v.push(e.0.to_string());
+         v.push(clean_word(&e.0.to_string()));
     }
-
-    println!("Markov created");
-    let mut c = 0;
-    for (k, v) in di {
-       println!("({},{}) -> {:?}", k.0, k.1, v);
-       c+=1;
-       if c == 10 {
-         break;
-       }
-    }
-
+    di
 }
 
-fn gen_markov() {
+fn gen_markov() -> HashMap<String, Vec<String>> {
     let filename = String::from("./data/train.txt");
     let contents = fs::read_to_string(filename).expect("Failed to open haiku");
     println!("Got coropra: {}", contents.len());
@@ -71,8 +63,31 @@ fn gen_markov() {
         prev = w.clone();
         v.push(w);
     }
+    haikudict
+}
 
-    println!("Haiku dict: {}", haikudict.len());
+
+fn gen_markov1() -> HashMap<String, Vec<String>> {
+    let filename = String::from("./data/train.txt");
+    let co = fs::read_to_string(filename).expect("Failed to open haiku");
+    let contents = clean_word(&co);
+    println!("Got coropra: {}", contents.len());
+
+   
+    
+    let i1 = contents.split(" ");
+    let i2 = contents.split(" ").skip(1);
+    
+    let zipp:Vec<_> = i1.zip(i2).collect();
+    let mut hd: HashMap<String, Vec<String>> = HashMap::new();
+
+    for (prev, word) in zipp {
+        let mut v = hd.entry(clean_word(&prev.to_string())).or_insert(Vec::<String>::new());
+        v.push(clean_word(&word.to_string()));
+    }
+
+   hd
+    
 }
 
 fn reader(mydict: HashMap<String, usize>) {
@@ -88,7 +103,23 @@ fn reader(mydict: HashMap<String, usize>) {
 }
 
 fn main() {
+    let m0 = gen_markov1();
+
     let mydict = read_cmu(String::from("./data/cmudict.dict"));
-    //gen_markov();
-    alt_markov();
+    let m1 = gen_markov();
+    let m2 = alt_markov();
+
+    println!("len {}", m0.keys().len()); 
+    let mut rng = rand::thread_rng();
+    let ind = rng.gen_range(0, m0.keys().len());
+  
+    let random_word = m0.keys().skip(ind).next().unwrap();
+    println!("Random word: {}", random_word);
+    let conts: Vec<String> = m0.get(random_word).unwrap().to_vec();
+    println!("Possible continuations: {:?}", conts);
+
+    let cont = conts.choose(&mut rng).unwrap(); 
+    println!("Sentence: {} {}", random_word, cont);
+   
+    
 }
