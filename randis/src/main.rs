7fd885445aed77b4f6ddac4f1353dcfa465a1ae3
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-26 07:11:56 +0200

    mc doors

--- randis/src/main.rs
+++ randis/src/main.rs
@@ -1,11 +1,9 @@
 use rand::Rng;
 use std::collections::HashSet;
 
-fn main() {
-
-   let mut rng = rand::thread_rng();
+fn mc_dice(trails: usize) -> f64 {
+ let mut rng = rand::thread_rng();
    let mut suc = 0;
-   let trails = 100000;
 
    for _ in 0..trails {
        let mut faces = HashSet::new();
@@ -17,10 +15,33 @@ fn main() {
        if faces.len() == 6 {
           suc+=1;
        }
-       
- 
     }
 
-    println!("Prob {}", suc as f64 / trails as f64);
-   
+    let prob = suc as f64 / trails as f64;
+    println!("Prob {}", prob);
+    prob
+}
+
+fn main() {
+   let prob = mc_dice(10000);
+
+   let mut rng = rand::thread_rng();
+   let trails = 10000;
+
+   let mut wins = 0;
+   let mut change_wins = 0;
+   for _ in 0..trails {
+       let player_door:usize = rng.gen_range(0, 3);
+       let price_door:usize = rng.gen_range(0, 3);
+ 
+
+       if player_door == price_door {
+            wins+=1;
+       } else {
+            change_wins +=1;
+       }
+   }
+
+   println!("Player wins: {}", wins as f64 / trails as f64);
+   println!("Change wins: {}", change_wins as f64 / trails as f64);
 }
