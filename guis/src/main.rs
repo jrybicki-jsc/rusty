3dd966480356e76eed536c91058e78bcd8b67fb5
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-08-29 10:33:23 +0200

    counters

--- guis/src/main.rs
+++ guis/src/main.rs
@@ -18,29 +18,39 @@ fn main() {
     println!("res: {:?}", res)
 }
 
+fn gen_game(wins: usize, loses: usize) -> Game {
+   let mut rng = rand::thread_rng();
+   let goat_door: usize = rng.gen_range(1, 4);
+        let price_door: usize = rng.gen_range(1, 4);
+
+        let game = Game {
+            goat_door: goat_door,
+            price_door: price_door,
+            wins: wins,
+            losses: loses,
+        };
+
+   game
+}
+
 fn invoke_handler(wv: &mut WebView<std::vec::Vec<Game>>, arg: &str) -> WVResult {
     println!("Arg is {}", arg);
+
+    let mut rng = rand::thread_rng();
 
     if arg == "exit" {
         println!("exiting!");
         wv.exit();
     }
 
-    if arg == "init" {
-        let mut rng = rand::thread_rng();
-        println!("initializing...");
-        let goat_door: usize = rng.gen_range(1, 4);
-        let price_door: usize = rng.gen_range(1, 4);
-
-        let game = Game {
-            goat_door: goat_door,
-            price_door: price_door,
-            wins: 0,
-            losses: 0,
-        };
-        let mut data = &mut *wv.user_data_mut();
+    let mut data = &mut *wv.user_data_mut();
+    if data.len() == 0 {
+        println!("No game status found, generating new one!");
+        let game = gen_game(0, 0);
         data.push(game);
-    }
+     }
+
+    
     if arg.starts_with("door") {
         let ss = &arg.to_string()[4..];
         let door_nr = usize::from_str(ss).unwrap();
@@ -55,11 +65,18 @@ fn invoke_handler(wv: &mut WebView<std::vec::Vec<Game>>, arg: &str) -> WVResult 
                 println!("Price door is: {}", game.price_door);
                 if (door_nr == game.price_door) {
                     println!("We have a winner!");
+                    let g = gen_game(game.wins+1, game.losses);
+                    data.push(g);
                     wv.eval(&format!("winner()"));
                 } else if (door_nr == game.goat_door) {
+                    let g = gen_game(game.wins, game.losses+1);
+                    data.push(g);
                     wv.eval(&format!("looser()"));
                 } else {
                     println!("nothing...");
+                    let g = gen_game(game.wins, game.losses);
+                    data.push(g); 
+                    wv.eval(&format!("clean({}, {})", game.wins, game.losses));
                 }
             }
         }
