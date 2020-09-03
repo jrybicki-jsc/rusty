91280e56e65c894164dc61588bc3dd89b6370569
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-09-03 08:03:44 +0200

    ro

--- randis/src/main.rs
+++ randis/src/main.rs
@@ -22,8 +22,7 @@ fn mc_dice(trails: usize) -> f64 {
     prob
 }
 
-fn main() {
-   let prob = mc_dice(10000);
+fn doors() {
 
    let mut rng = rand::thread_rng();
    let trails = 10000;
@@ -44,4 +43,32 @@ fn main() {
 
    println!("Player wins: {}", wins as f64 / trails as f64);
    println!("Change wins: {}", change_wins as f64 / trails as f64);
+}
+
+
+fn main() {
+     let mut rng = rand::thread_rng();
+     let max_people = 40;
+     let runs = 1000;
+     
+     for people in 2..max_people {
+         let mut shar =0;
+         for run in 0..runs {
+              let mut bdays = Vec::<usize>::new();
+              let mut bhash = HashSet::<usize>::new();
+              for i in 0..people {
+                   let bday:usize = rng.gen_range(0, 365);
+                   bdays.push(bday);
+                   bhash.insert(bday);
+              }
+
+              let diff = bdays.len() - bhash.len();
+              if diff >0 {
+                     shar+=1;
+              }
+         }
+         println!("{} people in a room: Probabilty shared: {}", people, shar as f64 / runs as f64);
+
+     }
+
 }
