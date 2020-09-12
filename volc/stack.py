3e01e0adf783c53fa3c3ca950d5b62461f4f2773
jrybicki-jsc <j.rybicki@fz-juelich.de>
2020-09-12 08:48:27 +0200

    cropandstack

--- volc/stack.py
+++ volc/stack.py
@@ -1,20 +1,31 @@
 import os
-import sys
-import shutil
-from PIL import Image, ImageOps
+from PIL import Image
 
-def main():
-    img_dir = 'res/video_frames'
-    files = os.listdir(img_dir)
-    with Image.open(os.path.join(img_dir, files[0])) as img:
-        gray = img.convert('L')
-        bw = gray.point(lambda x: 0 if x < 90 else 255)
-        box = bw.getbbox()
-        padded_box = (box[0]-20, box[1]-20, box[2]+20, box[3]+20)
-        cropped = img.crop(padded_box)
-        scalled = ImageOps.fit(cropped, (860, 860), Image.LANCZOS, 0, (0.5, 0.5))
-        scalled.save('bl.jpg', 'JPEG')
+os.chdir('cropped')
+images = os.listdir()
 
+red_data =[]
+green_data = []
+blue_data = []
 
-if __name__=="__main__":
-    main()
+for image in images:
+    with Image.open(image) as img:
+         if image ==images[0]:
+            img_size = img.size
+
+         red_data.append(list(img.getdata(0)))
+         green_data.append(list(img.getdata(1)))
+         blue_data.append(list(img.getdata(2)))
+
+avg_red = [round(sum(x) / len(red_data)) for x in zip(*red_data)]
+avg_blue = [round(sum(x) / len(blue_data)) for x in zip(*blue_data)]
+avg_green = [round(sum(x) / len(green_data)) for x in zip(*green_data)]
+
+merged_data = [(x) for x in zip(avg_red, avg_green, avg_blue)]
+stacked = Image.new('RGB', (img_size))
+stacked.putdata(merged_data)
+stacked.show()
+
+os.chdir('..')
+stacked.save('stacked.tiff', 'TIFF')
+ 
